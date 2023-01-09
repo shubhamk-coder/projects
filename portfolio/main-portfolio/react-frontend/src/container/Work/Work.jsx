d5182391d1908cc5f89dc966612944ea1af7cada
Shubham Kamble <shubhamkamble8983@gmail.com>
2023-01-09 22:44:30 +0530

    updated work item

--- portfolio/main-portfolio/react-frontend/src/container/Work/Work.jsx
+++ portfolio/main-portfolio/react-frontend/src/container/Work/Work.jsx
@@ -43,17 +43,19 @@ const Work = () => {
       </h2>
 
       <div className="app__work-filter">
-        {["Web App", "React JS", "Landing Pages", "All"].map((item, index) => (
-          <div
-            key={index}
-            onClick={() => handleWorkFilter(item)}
-            className={`app__work-filter-item app__flex p-text ${
-              activeFilter === item ? "item-active" : ""
-            }`}
-          >
-            {item}
-          </div>
-        ))}
+        {["Web App", "Vanilla JS", "Landing Pages", "All"].map(
+          (item, index) => (
+            <div
+              key={index}
+              onClick={() => handleWorkFilter(item)}
+              className={`app__work-filter-item app__flex p-text ${
+                activeFilter === item ? "item-active" : ""
+              }`}
+            >
+              {item}
+            </div>
+          )
+        )}
       </div>
 
       <motion.div
