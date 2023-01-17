8ac99a2ce18627b8d3252c8c375ed5655e36c1d5
Shubham Kamble <shubhamkamble8983@gmail.com>
2023-01-17 22:32:12 +0530

    Initial Commit

---
+++ Other Projects/Shopping Cart/frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">e-kart</a>
+      </header>
+      <main>list products</main>
+    </div>
+  );
+}
+
+export default App;
