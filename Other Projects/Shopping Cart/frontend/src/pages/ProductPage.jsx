b06f7186a2ec87fb3209cd7e82e1d47212f48e1e
Shubham Kamble <shubhamkamble8983@gmail.com>
2023-01-18 23:21:19 +0530

    Added Product Page

---
+++ Other Projects/Shopping Cart/frontend/src/pages/ProductPage.jsx
@@ -0,0 +1,46 @@
+import React, { useEffect, useReducer } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "FETCH_REQUEST":
+      return { ...state, loading: true };
+    case "FETCH_SUCCESS":
+      return { ...state, product: action.payload, loading: false };
+    case "FETCH_FAIL":
+      return { ...state, loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
+function ProductPage() {
+  const params = useParams();
+  const { Handle } = params;
+  const [{ loading, error, product }, dispatch] = useReducer(reducer, {
+    product: [],
+    loading: true,
+    error: "",
+  });
+  useEffect(() => {
+    const fetchData = async () => {
+      dispatch({ type: "FETCH_REQUEST" });
+      try {
+        const result = await axios.get(`/api/products/handle/${Handle}`);
+        dispatch({ type: "FETCH_SUCCESS", payload: result.data });
+      } catch (err) {
+        dispatch({ type: "FETCH_FAIL", payload: err.message });
+      }
+    };
+    fetchData();
+  }, [Handle]);
+  return loading ? (
+    <div>Loading...</div>
+  ) : error ? (
+    <div>{error}</div>
+  ) : (
+    <div>{product.name}</div>
+  );
+}
+
+export default ProductPage;
