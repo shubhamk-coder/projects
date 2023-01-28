5305bd49ba0f19e732c777692cb677400189792f
Shubham Kamble <shubhamkamble8983@gmail.com>
2023-01-28 17:54:49 +0530

    added build script

--- Other Projects/Shopping Cart/backend/server.js
+++ Other Projects/Shopping Cart/backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import productRouter from "./routes/productRoutes.js";
+import path from "path";
 
 dotenv.config();
 mongoose
@@ -11,10 +12,19 @@ mongoose
 
 const app = express();
 
-const port = process.env.PORT || 5000;
-
 app.use("/api/products", productRouter);
 
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, "/frontend/build")));
+app.get("*", (req, res) =>
+  res.sendFile(path.join(__dirname, "/frontend/build/index.html"))
+);
+
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
+});
+
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`serve at http://localhost:${port}`);
 });
