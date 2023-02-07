5ed1a89675a0a1e25cad24fb850bb19ef4ac7c0e
Shubham Kamble <shubhamkamble8983@gmail.com>
2023-02-08 02:10:40 +0530

    Added backend login with database

--- dalle-clone/frontend/src/pages/Home.jsx
+++ dalle-clone/frontend/src/pages/Home.jsx
@@ -15,6 +15,38 @@ const Home = () => {
   const [allPosts, setAllPosts] = useState(null);
   const [searchText, setSearchText] = useState("");
 
+  useEffect(() => {
+    const fetchPosts = async () => {
+      setLoading(true);
+      try {
+        const response = await fetch("http://localhost:8080/api/v1/post", {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+        if (response.ok) {
+          const result = await response.json();
+
+          setAllPosts(result.data.reverse());
+        }
+      } catch (error) {
+        alert(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchPosts();
+  }, []);
+
+  const handleSearchChange = (e) => {
+    setSearchText(e.target.value);
+
+    // setTimeout((e)=> {
+    //   const searchResult = await fetch("http://localhost:8080/
+    // })
+  };
+
   return (
     <section className="max-w-7xl mx-auto">
       <div>
@@ -48,7 +80,7 @@ const Home = () => {
               {searchText ? (
                 <RenderCards data={[]} title="No search results found" />
               ) : (
-                <RenderCards data={[]} title="No posts found" />
+                <RenderCards data={allPosts} title="No posts found" />
               )}
             </div>
           </>
--- dalle-clone/frontend/src/utils/index.js
+++ dalle-clone/frontend/src/utils/index.js
@@ -1,3 +1,4 @@
+import FileSaver from "file-saver";
 import { surpriseMePrompts } from "../constants";
 
 export function getRandomPrompt(prompt) {
@@ -7,4 +8,8 @@ export function getRandomPrompt(prompt) {
   if (randomPrompt === prompt) return getRandomPrompt(prompt);
 
   return randomPrompt;
+}
+
+export async function downloadImage(_id, photo) {
+  FileSaver.saveAs(photo, `download-${_id}.jpg`);
 }
