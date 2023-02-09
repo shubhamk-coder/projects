76d9e4b8c33d342ce1fa19d72505ad1005f9a9f8
Shubham Kamble <shubhamkamble8983@gmail.com>
2023-02-10 00:49:38 +0530

    added package in the root folder

--- dalle-clone/backend/index.js
+++ dalle-clone/backend/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import * as dotenv from "dotenv";
 import cors from "cors";
+import path from "path";
 
 import connectDB from "./mongodb/connect.js";
 import postRoutes from "./routes/postRoutes.js";
@@ -14,6 +15,12 @@ app.use(express.json({ limit: "50mb" }));
 
 app.use("/api/v1/post", postRoutes);
 app.use("/api/v1/dalle", dalleRoutes);
+
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+app.get("*", (req, res) =>
+  res.sendFile(path.join(__dirname, "/frontend/dist/index.html"))
+);
 
 app.get("/", async (req, res) => {
   res.send("Hello from DALL-E!");
